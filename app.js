9a71cc32c80b1312468744c07e110bc48fa46b3d
sdmurcia <sergiomurcia17@gmail.com>
2022-03-19 07:39:00 -0500

    Modifiqué el script para que aparezca el footer

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-app.use(express.static("public"));
-app.listen(3050,(req,res)=>{
-    console.log("Servidor corriendo");
+const express=require('express');
+const app=express();
+const path=require('path');
+const port = 3050;
+app.use(express.static('./public'));
+app.listen(port, () => {
+  console.log(`Está corriendo el servidor en el puerto: ${port}`)
 })
-app.get("/home",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./views/index.html'))
+})
+app.get('/footer', (req, res) => {
+  res.sendFile(path.join(__dirname,'./views/footer.html'))
 })
