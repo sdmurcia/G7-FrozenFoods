58bc4de27a98c48f53a345028be2e9a94b64c993
sdmurcia <sergiomurcia17@gmail.com>
2022-03-28 20:54:14 -0500

    actualizando archivos varios

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 const port = 3050;
 app.use(express.static('./public'));
 app.listen(port, () => {
   console.log(`Está corriendo el servidor en el puerto: ${port}`)
 })
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'./views/index.html'))
+  res.sendFile(path.join(__dirname, './views/index.html'))
 })
 app.get('/footer', (req, res) => {
-  res.sendFile(path.join(__dirname,'./views/footer.html'))
+  res.sendFile(path.join(__dirname, './views/footer.html'))
 })
 app.get('/header', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/header.html'))
-  })
-  app.get('/sign-in', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/sign-in.html'))
-  })
+  res.sendFile(path.join(__dirname, './views/header.html'))
+})
+app.get('/sign-in', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/sign-in.html'))
+})
+app.get('/sign-up', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/sign-up.html'))
+})
